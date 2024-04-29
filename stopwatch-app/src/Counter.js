9cd3cd77b939389ab2b5ab6ef382b74209a53256
Baturenes17 <kilicbatur614@gmail.com>
2024-04-29 04:55:54 +0300

    update
    update

--- stopwatch-app/src/Counter.js
+++ stopwatch-app/src/Counter.js
@@ -1,46 +1,99 @@
 import { useEffect, useState } from "react"
-import { StyleSheet, View,Text,Image } from "react-native"
+import { StyleSheet, View, Text, Image, Pressable } from "react-native"
+import { Entypo } from '@expo/vector-icons';
+import { FontAwesome6 } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 
 export const Counter = () => {
 
-    const [milliSeconds,setMilliSeconds] = useState(0);
-    const [seconds,setSeconds] = useState(0);
-    const [minutes,setMinutes] = useState(0);
+    const [milliSeconds, setMilliSeconds] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const [records, setRecords] = useState([]);
 
     var timer;
 
-    useEffect(()=>{
-        timer = setInterval(()=>{
-            setMilliSeconds(milliSeconds+1);
-            if(milliSeconds == 99){
-                setSeconds(seconds+1);
-                setMilliSeconds(0);
-            }
-            if(seconds==59){
-                setMinutes(minutes+1);
-                setSeconds(0);
-            }
-        },10)
+    useEffect(() => {
 
-        return()=>clearInterval(timer);
+        if (isRunning) {
+            timer = setInterval(() => {
+                setMilliSeconds(milliSeconds + 1);
+                if (milliSeconds == 99) {
+                    setSeconds(seconds + 1);
+                    setMilliSeconds(0);
+                }
+                if (seconds == 60) {
+                    setMinutes(minutes + 1);
+                    setSeconds(0);
+                }
+            }, 2)
+
+            return () => clearInterval(timer);
+        }
+
     })
 
-    return(
+
+    const startTimer = () => {
+        if (isRunning == false) {
+            setIsRunning(true);
+        } else {
+            setIsRunning(false);
+        }
+    }
+
+    const reset = () => {
+        setIsRunning(false);
+        setMilliSeconds(0);
+        setSeconds(0);
+        setMinutes(0);
+    }
+
+    const saveRecords = () => {
+        setRecords([...records, `${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}:${milliSeconds < 10 ? "0" + milliSeconds : milliSeconds}`])
+    }
+
+    const cleanRecords = () => {
+        setRecords([]);
+    }
+
+    return (
         <View style={styles.body} >
 
             <View style={styles.timerImg} >
-                <Image source={require("../assets/counter.png")} style={{width:175,height:175}} />
+                <Image source={require("../assets/counter.png")} style={{ width: 175, height: 175 }} />
             </View>
 
             <View style={styles.counter} >
-                <Text style={styles.counterText} > {minutes<10 ? "0" + minutes : minutes} : {seconds <10 ? "0" + seconds : seconds} : {milliSeconds} </Text>
+                <Text style={styles.counterText} > {minutes < 10 ? "0" + minutes : minutes} : {seconds < 10 ? "0" + seconds : seconds} : {milliSeconds < 10 ? "0" + milliSeconds : milliSeconds} </Text>
             </View>
 
             <View style={styles.records} >
-
+                    {records.map((records, index) => (
+                        <Text style={{ margin: 5, fontSize: 30 }} key={index} >{records}</Text>
+                    ))}
             </View>
 
             <View style={styles.buttonsArea} >
+                <Pressable style={{ flex: 1 }} onPress={() => saveRecords()}>
+                    <View style={styles.buttons} >
+                        <Feather name="check-circle" size={40} color="black" />
+                    </View>
+                </Pressable>
+
+
+                <Pressable style={{ flex: 1 }} onPress={() => startTimer()} >
+                    <View style={styles.buttons} >
+                        <Entypo name="stopwatch" size={40} color="black" />
+                    </View>
+                </Pressable>
+
+                <Pressable style={{ flex: 1 }} onPress={() => [reset(), cleanRecords()]}>
+                    <View style={styles.buttons} >
+                        <FontAwesome6 name="creative-commons-zero" size={40} color="black" />
+                    </View>
+                </Pressable>
 
             </View>
         </View>
@@ -48,33 +101,46 @@ export const Counter = () => {
 }
 
 const styles = StyleSheet.create({
-    body:{
-        flex:1,
-        width:"100%"
+    body: {
+        flex: 1,
+        width: "100%"
     },
-    timerImg:{
-        flex:2,
+    timerImg: {
+        flex: 2,
         //backgroundColor:"red",
-        justifyContent:"center",
-        alignItems:"center",
-        paddingTop:60
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 60
     },
-    counter:{
-        flex:1,
+    counter: {
+        flex: 1,
         //backgroundColor:"green",
-        justifyContent:"center",
-        alignItems:"center"
+        justifyContent: "center",
+        alignItems: "center"
     },
-    counterText:{
-        fontSize:25,
-        fontWeight:"500"
+    counterText: {
+        fontSize: 25,
+        fontWeight: "500"
     },
-    records:{
-        flex:4,
-        backgroundColor:"blue"
+    records: {
+        flex: 4,
+        justifyContent: "flex-start",
+        alignItems:"flex-end",
+        paddingRight:20,
+        paddingTop:20
+        //backgroundColor: "blue"
     },
-    buttonsArea:{
-        flex:1,
-        backgroundColor:"purple"
+    buttonsArea: {
+        flex: 0.7,
+        //backgroundColor: "purple",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    buttons: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100%",
     }
 })
